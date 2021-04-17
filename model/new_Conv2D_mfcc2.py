705c0c522758bce14ba6bd091c94cbc5e3295067
kimjh1753@naver.com <kimjh1753@naver.com>
2021-04-17 11:40:47 +0900

    challenge

---
+++ model/new_Conv2D_mfcc2.py
@@ -0,0 +1,154 @@
+# (1차 진행) mel과 mfcc를 돌려서 확인하기
+
+import numpy as np
+import librosa
+import sklearn
+import datetime
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential, load_model, Model
+from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, AveragePooling2D, Dropout, Activation, Flatten, Add, Input, Concatenate
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, TensorBoard
+from tensorflow.python.keras.callbacks import ModelCheckpoint
+def normalize(x, axis=0):
+    return sklearn.preprocessing.minmax_scale(x, axis=axis)
+
+start_now = datetime.datetime.now()
+
+# 데이터 불러오기
+f_ds = np.load('C:/nmb/nmb_data/npy/F_newtest_mfccs.npy')
+m_ds = np.load('C:/nmb/nmb_data/npy/M_newtest_mfccs.npy')
+f_lb = np.load('C:/nmb/nmb_data/npy/F_newtest_label_mfccs.npy')
+m_lb = np.load('C:/nmb/nmb_data/npy/M_newtest_label_mfccs.npy')
+
+x = np.concatenate([f_ds, m_ds], 0)
+y = np.concatenate([f_lb, m_lb], 0)
+print(x.shape, y.shape) # (2141, 20, 862) (2141,)
+
+# 전처리
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, shuffle=True, train_size=0.8, random_state=66
+)
+
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
+
+print(x_train.shape, y_train.shape) # (1712, 20, 862, 1) (1712,)
+print(x_test.shape, y_test.shape)   # (429, 20, 862, 1) (429,)
+
+# 모델 구성
+model = Sequential()
+
+def residual_block(x, filters, conv_num=3, activation='relu'):  # ( input, output node, for 문 반복 횟수, activation )
+    # Shortcut
+    s = Conv2D(filters, 1, padding='same')(x)
+    for i in range(conv_num - 1):
+        x = Conv2D(filters, 3, padding='same')(x)
+        x = Activation(activation)(x)
+    x = Conv2D(filters, 3, padding='same')(x)
+    # x = Add()([x,s])
+    x = Concatenate(axis=-1)([x, s])
+    x = Activation(activation)(x)
+    return MaxPool2D(pool_size=2, strides=1)(x)
+
+def build_model(input_shape, num_classes):
+    inputs = Input(shape=input_shape, name='input')
+
+    x = residual_block(inputs, 16, 2)
+    x = residual_block(x, 32, 2)
+    x = residual_block(x, 64, 3)
+    x = residual_block(x, 128, 3)
+    x = residual_block(x, 128, 23)
+
+    # x = residual_block(inputs, 1024, 2)
+    # x = residual_block(x, 512, 2)
+    # x = residual_block(x, 512, 3)
+    # x = residual_block(x, 256, 3)
+    # x = residual_block(x, 256, 3)
+
+    x = AveragePooling2D(pool_size=3, strides=3)(x)
+    x = Flatten()(x)
+    x = Dense(256, activation="relu")(x)
+    x = Dense(128, activation="relu")(x)
+
+    outputs = Dense(num_classes, activation='softmax', name="output")(x)
+    
+    return Model(inputs=inputs, outputs=outputs)
+
+model = build_model(x_train.shape[1:], 2)
+print(x_train.shape[1:])    # (20, 862)
+
+model.summary()
+
+# Total params: 50,627,506
+# Trainable params: 50,627,506
+# Non-trainable params: 0
+
+# 컴파일, 훈련
+model.compile(optimizer='adam', loss="sparse_categorical_crossentropy", metrics=['acc'])
+es = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True, verbose=1)
+lr = ReduceLROnPlateau(monitor='val_loss', vactor=0.5, patience=10, verbose=1)
+path = 'C:/nmb/nmb_data/h5/new_Conv2D_mfcc.h5'
+mc = ModelCheckpoint(path, monitor='val_loss', verbose=1, save_best_only=True)
+tb = TensorBoard(log_dir='C:/nmb/nmb_data/graph',histogram_freq=0, write_graph=True, write_images=True)
+history = model.fit(x_train, y_train, epochs=300, batch_size=16, validation_split=0.2, callbacks=[es, lr, mc, tb])
+
+# 시각화
+# plt.plot(history.history['loss'])
+# plt.plot(history.history['val_loss'])
+# plt.plot(history.history['acc'])
+# plt.plot(history.history['val_acc'])
+
+# plt.title('loss & acc')
+# plt.xlabel('epoch')
+# plt.ylabel('loss & acc')
+# plt.legend(['train_loss', 'val_loss', 'train_acc', 'val_acc'])
+# plt.show()
+
+# 평가, 예측
+model.load_weights('C:/nmb/nmb_data/h5/new_Conv2D_mfcc.h5')
+
+result = model.evaluate(x_test, y_test, batch_size=16)
+print("loss : ", result[0])
+print("acc : ", result[1])
+
+pred_pathAudio = 'C:/nmb/nmb_data/pred_voice/'
+files = librosa.util.find_files(pred_pathAudio, ext=['wav'])
+files = np.asarray(files)
+for file in files:   
+    y, sr = librosa.load(file, sr=22050) 
+    mfccs = librosa.feature.mfcc(y, sr=sr, n_mfcc=20, n_fft=512, hop_length=128)
+    pred_mfccs = normalize(mfccs, axis=1)
+    pred_mfccs = pred_mfccs.reshape(1, pred_mfccs.shape[0], pred_mfccs.shape[1])
+    y_pred = model.predict(pred_mfccs)
+    y_pred_label = np.argmax(y_pred)
+    if y_pred_label == 0 :                   
+        print(file,(y_pred[0][0])*100, '%의 확률로 여자입니다.')
+    else:                               
+        print(file,(y_pred[0][1])*100, '%의 확률로 남자입니다.')
+
+end_now = datetime.datetime.now()
+time = end_now - start_now
+print("time >> " , time)    # time >>  0:00:33.975135
+
+# loss :  0.0
+# acc :  1.0
+# C:\nmb\nmb_data\pred_voice\FY1.wav 100.0 %의 확률로 남자입니다.
+# C:\nmb\nmb_data\pred_voice\MZ1.wav 100.0 %의 확률로 남자입니다.
+# C:\nmb\nmb_data\pred_voice\friendvoice_F4.wav 100.0 %의 확률로 남자입니다.
+# C:\nmb\nmb_data\pred_voice\friendvoice_M3.wav 100.0 %의 확률로 남자입니다.
+# C:\nmb\nmb_data\pred_voice\friendvoice_M4.wav 100.0 %의 확률로 남자입니다.
+# C:\nmb\nmb_data\pred_voice\friendvoice_M5.wav 100.0 %의 확률로 남자입니다.
+# C:\nmb\nmb_data\pred_voice\friendvoice_M6.wav 100.0 %의 확률로 남자입니다.
+# C:\nmb\nmb_data\pred_voice\friendvoice_M7.wav 100.0 %의 확률로 남자입니다.
+# C:\nmb\nmb_data\pred_voice\testvoice_F1(clear).wav 100.0 %의 확률로 남자입니다.
+# C:\nmb\nmb_data\pred_voice\testvoice_F1_high(clear).wav 100.0 %의 확률로 남자입니다.
+# C:\nmb\nmb_data\pred_voice\testvoice_F2(clear).wav 100.0 %의 확률로 남자입니다.
+# C:\nmb\nmb_data\pred_voice\testvoice_F3(clear).wav 100.0 %의 확률로 남자입니다.
+# C:\nmb\nmb_data\pred_voice\testvoice_M1(clear).wav 100.0 %의 확률로 남자입니다.
+# C:\nmb\nmb_data\pred_voice\testvoice_M2(clear).wav 100.0 %의 확률로 남자입니다.
+# C:\nmb\nmb_data\pred_voice\testvoice_M2_low(clear).wav 100.0 %의 확률로 남자입니다.
+# time >>  0:00:58.534501
+# 정답률 : 9/15
+
