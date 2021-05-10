1a7f92a4e13f587b7ca25ae42e1c1e978bf2fd6b
kimjh1753@naver.com <kimjh1753@naver.com>
2021-05-10 13:57:27 +0900

    challenge

--- 1s_model/vgg19_adadelta_1.py
+++ 1s_model/vgg19_adadelta_1.py
@@ -42,15 +42,15 @@ model = VGG19(
 model.summary()
 # model.trainable = False
 
-model.save('C:/nmb/nmb_data/h5/5s/vgg19/vgg19_adam_2.h5')
+model.save('C:/nmb/nmb_data/h5/5s/vgg19/vgg19_adadelta_1.h5')
 
 # 컴파일, 훈련
-op = Adam(lr=1e-3)
+op = Adadelta(lr=1e-3)
 batch_size = 4
 
 es = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True, verbose=1)
 lr = ReduceLROnPlateau(monitor='val_loss', vactor=0.5, patience=10, verbose=1)
-path = 'C:/nmb/nmb_data/h5/5s/vgg19/vgg19_adam_2.h5'
+path = 'C:/nmb/nmb_data/h5/5s/vgg19/vgg19_adadelta_1.h5'
 mc = ModelCheckpoint(path, monitor='val_loss', verbose=1, save_best_only=True)
 tb = TensorBoard(log_dir='C:/study/graph/'+ start_now.strftime("%Y%m%d-%H%M%S") + "/",histogram_freq=0, write_graph=True, write_images=True)
 
@@ -58,8 +58,8 @@ model.compile(optimizer=op, loss="sparse_categorical_crossentropy", metrics=['ac
 history = model.fit(x_train, y_train, epochs=1000, batch_size=batch_size, validation_split=0.2, callbacks=[es, lr, mc, tb])
 
 # 평가, 예측
-# model = load_model('C:/nmb/nmb_data/h5/5s/vgg19/vgg19_adam_2.h5')
-model.load_weights('C:/nmb/nmb_data/h5/5s/vgg19/vgg19_adam_2.h5')
+# model = load_model('C:/nmb/nmb_data/h5/5s/vgg19/vgg19_adadelta_1.h5')
+model.load_weights('C:/nmb/nmb_data/h5/5s/vgg19/vgg19_adadelta_1.h5')
 result = model.evaluate(x_test, y_test, batch_size=8)
 print("loss : {:.5f}".format(result[0]))
 print("acc : {:.5f}".format(result[1]))
