5c8c7937cf95f6029f7e7fb352eeaadd51c35ebb
kimjh1753@naver.com <kimjh1753@naver.com>
2021-05-29 23:12:30 +0900

    challenge

--- 5s_new_model_0602/ml_lgbm.py
+++ 5s_new_model_0602/ml_lgbm.py
@@ -52,7 +52,7 @@ model.fit(x_train, y_train, verbose=1) #, callbacks=[tb])
 # accuracy
 train_sizes, train_scores_model, test_scores_model = \
     learning_curve(model, x_train[:100], y_train[:100], train_sizes=np.linspace(0.1, 1.0, 10),
-                   scoring="accuracy", cv=8, shuffle=True, random_state=42)
+                   scoring="accuracy", cv=8, shuffle=True, random_state=42, verbose=1)
 
 train_scores_mean = np.mean(train_scores_model, axis=1)
 train_scores_std = np.std(train_scores_model, axis=1)
@@ -60,9 +60,9 @@ test_scores_mean = np.mean(test_scores_model, axis=1)
 test_scores_std = np.std(test_scores_model, axis=1)
 
 # log loss
-train_sizes, train_scores_model, test_scores_model = \
-    learning_curve(model, x_train[:100], y_train[:100], train_sizes=np.linspace(0.1, 1.0, 10),
-                   scoring='neg_log_loss', cv=8, shuffle=True, random_state=42)
+# train_sizes, train_scores_model, test_scores_model = \
+#     learning_curve(model, x_train[:100], y_train[:100], train_sizes=np.linspace(0.1, 1.0, 10),
+#                    scoring='neg_log_loss', cv=8, shuffle=True, random_state=42)
 
 # accuracy
 plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
@@ -71,8 +71,8 @@ plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
                  label="validation score")
 
 # log loss
-plt.plot(train_sizes, -train_scores_model.mean(1), 'o-', color="r", label="log_loss")
-plt.plot(train_sizes, -test_scores_model.mean(1), 'o-', color="g", label="val log_loss")
+# plt.plot(train_sizes, -train_scores_model.mean(1), 'o-', color="r", label="log_loss")
+# plt.plot(train_sizes, -test_scores_model.mean(1), 'o-', color="g", label="val log_loss")
 
 plt.xlabel("Train size")
 # plt.ylabel("Log loss")
